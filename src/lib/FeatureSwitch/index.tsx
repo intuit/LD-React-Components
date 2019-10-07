3086490b83738d8d9b532321798807a76e816a02
cueo <mohit.ritanil@gmail.com>
2019-10-07 09:57:19 +0530

    Fix TS2367 : No overlap for comparison Cast element to type `any`

--- src/lib/FeatureSwitch/index.tsx
+++ src/lib/FeatureSwitch/index.tsx
@@ -28,7 +28,7 @@ const FeatureSwitch: React.FC<IComponentProps> = (props) => {
 
   React.Children.forEach(children, element => {
     // if the Component is FeatureCase and break is false, compare the feature flag and render the element if its true
-    if (React.isValidElement(element) && element.type === FeatureCase && !breakIt) {
+    if (React.isValidElement(element) && (element as any).type === FeatureCase && !breakIt) {
       // TODO use proper type cast here once they are defined
       const { condition, allowBreak } = (element as any).props;
       if ((appFlags[flagKey] && appFlags[flagKey].value) === condition) {
@@ -37,12 +37,12 @@ const FeatureSwitch: React.FC<IComponentProps> = (props) => {
       }
     }
     // if its Default and it is not breaked yet, render the element.
-    if (React.isValidElement(element) && element.type === FeatureDefault && !breakIt) {
+    if (React.isValidElement(element) && (element as any).type === FeatureDefault && !breakIt) {
       childArray.push(element);
     }
   });
 
   return <React.Fragment>{childArray}</React.Fragment>;
-}
+};
 
 export default FeatureSwitch;
