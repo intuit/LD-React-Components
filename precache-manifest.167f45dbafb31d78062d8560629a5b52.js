4da3307ece691ac09216e5b5b4a29829ac55efd7
poorpaddy <dave@cmscode.com>
2021-02-25 13:52:00 +0000

    bbaa4ee

---
+++ precache-manifest.167f45dbafb31d78062d8560629a5b52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ce0fc7b7af028d840a6d98e312c927",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.55676858895b9568e52b.bundle.js"
+  },
+  {
+    "url": "runtime~main.55676858895b9568e52b.bundle.js"
+  },
+  {
+    "revision": "b7dbef918cab14d8e0623be2c92e38f4",
+    "url": "static/media/config.b7dbef91.yml"
+  },
+  {
+    "url": "vendors~main.55676858895b9568e52b.bundle.js"
+  },
+  {
+    "url": "vendors~main.55676858895b9568e52b.bundle.js.LICENSE.txt"
+  }
+]);
